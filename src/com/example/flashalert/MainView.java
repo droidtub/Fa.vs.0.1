da5aad17121ee3b3f5a9197053bc13901a5429d1
Hanh Nguyen <droidtub@gmail.com>
2015-08-02 00:28:48 +0700

    Add Project

---
+++ src/com/example/flashalert/MainView.java
@@ -0,0 +1,48 @@
+package com.example.flashalert;
+
+import com.example.flashalert.controller.MainController;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class MainView {
+	private MainController mController;
+	private ActionBarActivity mActivity;
+	protected Context mContext;
+	protected FrameLayout mMainLayout;
+	private Toolbar mToolbar;
+	private RecyclerView recyclerView;
+	private MainViewAdapter mAdapter;
+	
+	public MainView(MainController controller, ActionBarActivity activity){
+		mController = controller;
+		mActivity = activity;
+	}
+	
+	/*public void initializeUi(Context context, View view ){
+		mContext = context;
+		mMainLayout = (FrameLayout)view.findViewById(android.R.id.content);
+		LayoutInflater.from(mContext).inflate(R.layout.activity_main, mMainLayout);
+		
+		
+	}*/
+	
+	public void initializeUi(){
+		mActivity.setContentView(R.layout.activity_main);
+		mToolbar = (Toolbar)mActivity.findViewById(R.id.toolbar);
+		mActivity.setSupportActionBar(mToolbar);
+		mActivity.getSupportActionBar().setDisplayShowHomeEnabled(true);
+		
+		recyclerView = (RecyclerView)mActivity.findViewById(R.id.container_body);
+		recyclerView.setLayoutManager(new LinearLayoutManager(mActivity));
+		recyclerView.setHasFixedSize(true);
+		mAdapter = new MainViewAdapter(this);
+		recyclerView.setAdapter(mAdapter);
+	}
+}
---
+++ src/com/example/flashalert/MainViewAdapter.java
@@ -0,0 +1,41 @@
+package com.example.flashalert;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainViewAdapter extends RecyclerView.Adapter<MainViewAdapter.ViewHolder> {
+
+	private MainView mv;
+	
+	public MainViewAdapter(MainView mv){
+		this.mv = mv;
+	}
+
+	@Override
+	public int getItemCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void onBindViewHolder(ViewHolder arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ViewHolder onCreateViewHolder(ViewGroup arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	class ViewHolder extends RecyclerView.ViewHolder{
+
+		public ViewHolder(View itemView) {
+			super(itemView);
+			// TODO Auto-generated constructor stub
+		}
+		
+	}
+}
